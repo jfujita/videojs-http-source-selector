a53a68aef9953d7d79e2fc3a342a459b761ce7e3
Mike Griffith <bikegriffith@gmail.com>
2018-10-17 12:58:46 -0400

    style and standards cleanup

--- src/components/SourceMenuButton.js
+++ src/components/SourceMenuButton.js
@@ -2,131 +2,73 @@ import SourceMenuItem from './SourceMenuItem';
 
 const MenuButton = videojs.getComponent('MenuButton');
 
-// The videojs.extend function is used to assist with inheritance. In
-// an ES6 environment, `class SourceButton extends MenuButton` would work
-// identically.
 class SourceMenuButton extends MenuButton
 {
-  // The constructor of a component receives two arguments: the
-  // player it will be associated with and an object of options.
-  constructor(player, options)
-  {
-    // It is important to invoke the superclass before anything else,
-    // to get all the features of components out of the box!
+  constructor(player, options) {
     super(player, options);
+
     MenuButton.apply(this, arguments);
+
     var qualityLevels = this.player().qualityLevels();
+
     // Handle options: We accept an options.default value of ( high || low )
     // This determines a bias to set initial resolution selection.
-    if(options && options.default)
-    {
-      console.log("options.default: "+options.default);
-      console.log("this.player().qualityLevels(): ");
-      console.log(this.player().qualityLevels());
-      console.log("this.player().qualityLevels().length: ");
-      console.log(this.player().qualityLevels().length);
-      if(options.default == 'low')
-      {
-        for(var i = 0; i < this.player().qualityLevels().length; i++)
-        {
-          if(i == 0)
-          {
-            this.player().qualityLevels()[i].enabled = true;
-
-          }
-          else
-          {
-            this.player().qualityLevels()[i].enabled = false;
-
-          }
+    if (options && options.default) {
+      if (options.default == 'low') {
+        for (var i = 0; i < qualityLevels.length; i++) {
+          qualityLevels[i].enabled = (i == 0);
         }
-      }
-      else if(options.default = 'high')
-      {
-        for(var i = 0; i < this.player().qualityLevels().length; i++)
-        {
-          if(i == this.player().qualityLevels().length - 1)
-          {
-            this.player().qualityLevels()[i].enabled = true;
-
-          }
-          else
-          {
-            this.player().qualityLevels()[i].enabled = false;
-
-          }
+      } else if (options.default = 'high') {
+        for (var i = 0; i < qualityLevels.length; i++) {
+          qualityLevels[i].enabled = (i == (qualityLevels.length - 1));
         }
       }
     }
+
     // Bind update to qualityLevels changes
-    //qualityLevels.on(['change', 'addqualitylevel'], videojs.bind( this, this.update) );
+    //this.player().qualityLevels.on(['change', 'addqualitylevel'], videojs.bind( this, this.update) );
   };
 
-  // The `createEl` function of a component creates its DOM element.
-  createEl()
-  {
-    return videojs.dom.createEl('div',
-    {
-      // Prefixing classes of elements within a player with "vjs-"
-      // is a convention used in Video.js.
+  createEl() {
+    return videojs.dom.createEl('div', {
       className: 'vjs-http-source-selector vjs-menu-button vjs-menu-button-popup vjs-control vjs-button'
     });
   }
 
-  buildCSSClass()
-  {
+  buildCSSClass() {
     return MenuButton.prototype.buildCSSClass.call( this ) + ' vjs-icon-cog';
   }
 
-  update()
-  {
+  update() {
     return MenuButton.prototype.update.call(this);
   }
 
-  createItems()
-  {
+  createItems() {
     var menuItems = [];
-    var labels = (this.player.qualityLevels) || {};
+    var levels = this.player().qualityLevels();
 
-    for(var i = 0; i < this.player().qualityLevels().length; i++)
-    {
-      var j = this.player().qualityLevels().length - (i + 1);
+    for (var i = 0; i < levels.length; i++) {
+      var index = levels.length - (i + 1);
+      var selected = (index === levels.selectedIndex);
 
-      //console.log(this.player().qualityLevels()[j].id);
-      //console.log(this.player().qualityLevels()[j]);
-      var label = ""+j;
-      //Display height if height metadata is provided with the stream, else use bitrate
-      if(this.player().qualityLevels()[j].height)
-      {
-        label = this.player().qualityLevels()[j].height;
-      }
-      else if(this.player().qualityLevels()[j].bitrate)
-      {
-        label = Math.floor(this.player().qualityLevels()[j].bitrate / 1e3)+' kbps';
+      // Display height if height metadata is provided with the stream, else use bitrate
+      var label = `${index}`;
+      if (levels[index].height) {
+        label = `${levels[index].height}p`;
+      } else if (levels[index].bitrate) {
+        label = `${Math.floor(levels[index].bitrate / 1e3)} kbps`;
       }
 
-      menuItems.push(new SourceMenuItem(
-        this.player_,
-        {
-          label:label,
-          index: j,
-          selected: j === (this.player().qualityLevels().selectedIndex ? label : false)
-        })
-      );
+      menuItems.push(new SourceMenuItem(this.player_, {label, index, selected}));
     }
-    //If there are more than one quality levels, offer an 'auto' option
-    if(this.player().qualityLevels().length > 1)
-    {
-      menuItems.push(new SourceMenuItem(
-        this.player_,
-        {
-          label:'Auto',
-          index:this.player().qualityLevels().length,
-          selected: false
-        })
-      );
+
+    // If there are multiple quality levels, offer an 'auto' option
+    if (levels.length > 1) {
+      menuItems.push(new SourceMenuItem(this.player_, { label: 'Auto', index: levels.length, selected: false }));
     }
+
     return menuItems;
   }
 }
+
 export default SourceMenuButton;
--- src/components/SourceMenuItem.js
+++ src/components/SourceMenuItem.js
@@ -1,50 +1,38 @@
 const MenuItem = videojs.getComponent('MenuItem');
 
-
 class SourceMenuItem extends MenuItem
 {
-  constructor(player, options)
-  {
+  constructor(player, options) {
     super(player, options);
     options.selectable = true;
-    //var qualityLevels = this.player().qualityLevels();
-
-    //qualityLevels.on('change', videojs.bind(this, this.update));
-    //this.player.on('resolutionchange', videojs.bind(this, this.update));
   }
 
-  handleClick()
-  {
-    console.log("Change quality to "+this.options_.label);
-    console.log("qualityLevels: ");
-    console.log(this.player().qualityLevels());
-    this.selected_=true;
+  handleClick() {
+    var selected = this.options_;
+    console.log("Changing quality to:", selected.label);
+
+    this.selected_ = true;
     this.selected(true);
-    for(var i = 0; i < this.player().qualityLevels().length; i++)
-    {
-      //If this is the Auto option, enable all renditions for adaptive selection
-      if(this.options_.index == this.player().qualityLevels().length)
-      {
-        this.player().qualityLevels()[i].enabled = true;
-      }
-      else if(i == this.options_.index)
-      {
-        this.player().qualityLevels()[i].enabled = true;
-      }
-      else
-      {
-        this.player().qualityLevels()[i].enabled = false;
+
+    var levels = this.player().qualityLevels();
+    for(var i = 0; i < levels.length; i++) {
+      if (selected.index == levels.length) {
+        // If this is the Auto option, enable all renditions for adaptive selection
+        levels[i].enabled = true;
+      } else if (selected.index == i) {
+        levels.enabled = true;
+      } else {
+        levels.enabled = false;
       }
     }
   }
 
-  update()
-  {
-    var selection = this.player().qualityLevels().selectedIndex;
-    console.log(this.options_.index+" == "+selection);
-    console.log("menuItem.index === qualityLevels().selectedIndex");
+  update() {
+    var levels = this.player().qualityLevels();
+    var selection = levels.selectedIndex;
     this.selected(this.options_.index == selection);
     this.selected_ = (this.options_.index === selection);
   }
 }
+
 export default SourceMenuItem;
